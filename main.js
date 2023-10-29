d134320cd572b3dddf28d35d04f430a2be1b8e3c
dyanasadat-rejali <149334525+dyanasadat-rejali@users.noreply.github.com>
2023-10-29 22:37:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+if(navToggle){
+	navToggle.addEventListener('click', () =>{
+		navMenu.classList.add('show-menu')
+		})
+}
+
+if(navClose){
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show-menu')
+	})
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const skillsContent = document.getElementsByClassName('skills__content'),
+	skillsHeader = document.querySelectorAll('skills__header')
+
+function toggleSkills(){
+	let itemClass = this.parentNode.className
+
+	for(i = 0; i< skillsContent.length; i++){
+		skillsContent[i].className = 'skills__content skills__close'
+	}
+	if(itemClass=='skills__content skills__close'){
+		this.parentNode.className = 'skills__content skills__open'
+	}
+}
+
+skillsHeader.forEach((el) =>{
+	el.addEventListener('click', toggleSkills)
+})
+
+const tabs = document.querySelectorAll('[data-target]'),
+	tabContents = document.querySelectorAll('[data-content]')
+tabs.forEach(tab => {
+	tab.addEventListener('click', () =>{
+		tabContent.classList.remove('qualification__active')
+	})
+	target.classList.add('qualification__active')
+
+	tabs.forEach(tab =>{
+		tab.classList.remove('qualification__active')
+	})
+	tab.classList.add('qualification__active')
+})
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
